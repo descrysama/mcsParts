bd9ac17e5eb21a67c79a81077bdb65bd0096f9eb
descrysama <erwing2002@gmail.com>
2023-05-15 15:42:56 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from login import login;
-import csv;
+import openpyxl
 from webinit_ import initBrowser;
 from url_fournisseurs.utopya import utopya_urls;
 from single_links.utopya_single import utopya_single;
@@ -17,10 +17,21 @@ for single_phone in all_items :
     for single_item in single_phone :
         csv_file.append(single_item)
 
-with open('src/output/utopya.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
 
-    writer.writerow(['SKU', 'PRICE'])
-    for row in csv_file:
-        writer.writerow(row)
-file.close()
+
+workbook = openpyxl.Workbook()
+
+# Select the active worksheet
+worksheet = workbook.active
+
+# Write the header row to the worksheet
+worksheet.cell(row=1, column=1, value='SKU')
+worksheet.cell(row=1, column=2, value='PRICE')
+
+# Iterate over the rows in the CSV file and write them to the worksheet
+for row_number, row in enumerate(csv_file, start=2):
+    worksheet.cell(row=row_number, column=1, value=row[0])
+    worksheet.cell(row=row_number, column=2, value=row[1])
+
+# Save the workbook to a file
+workbook.save('src/output/utopya.xlsx')
