57936713449425c2c2eaa55028d9b3dda6d691d9
descrysama <erwing2002@gmail.com>
2023-05-16 17:40:30 +0200

    clear file

--- src/main.py
+++ src/main.py
@@ -2,11 +2,16 @@ from login import login;
 from webinit_ import initBrowser;
 from url_fournisseurs.utopya import utopya_urls;
 from single_links.utopya_single import utopya_single;
-
+import os
 
 driver = login(initBrowser(True))
 all_items = []
 csv_file = []
+
+file_path = '/var/www/html/output.xlsx'
+file_exists = os.path.isfile(file_path)
+if file_exists:
+    os.remove(file_exists)
 
 try: 
     for i, link in enumerate(utopya_urls) :
