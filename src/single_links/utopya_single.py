f2d87d9559600833011548e977ab276ba7d32d42
descrysama <erwing2002@gmail.com>
2023-05-16 10:58:07 +0200

    crash update

--- src/single_links/utopya_single.py
+++ src/single_links/utopya_single.py
@@ -8,26 +8,9 @@ def utopya_single(driver: webdriver, url):
     print(url)
     items = []
     no_sku_url = []
-    driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.pricing")))
-    products = driver.find_elements(by="css selector", value="div.product.details.product-item-details")
 
-    for product in products:
+    get_sku(items, no_sku_url, url, driver)
 
-        ##Recuperer le sku et si il n'y en a pas push l'url dans le tableau
-        try :
-            item = ['', '']
-            form = product.find_element(by="tag name", value="form")
-            item[0] = form.get_attribute("data-product-sku")
-            ## Recuperer le prix
-            pricing = product.find_element(by="css selector", value="div.pricing")
-            price_div = pricing.find_element(by="css selector", value="span.price.price-final_price.tax.weee")
-            item[1] = price_div.text.replace("€", ",")
-            items.append(item)
-        except: 
-            url = product.find_element(by="css selector", value="a.product-item-link.name")
-            no_sku_url.append(url.get_attribute("href"))
-            print("SKU not found placing it in the url array.")
         
     
     if(len(no_sku_url) > 0) :
@@ -51,3 +34,41 @@ def utopya_single(driver: webdriver, url):
     print(items)        
 
     return items
+
+
+def get_sku(items, no_sku_url, url, driver):
+
+    driver.get(url)
+    find_element_with_retry(items, no_sku_url, url, driver)
+    products = driver.find_elements(by="css selector", value="div.product.details.product-item-details")
+
+    for product in products:
+            ##Recuperer le sku et si il n'y en a pas push l'url dans le tableau
+        try :
+            item = ['', '']
+            form = product.find_element(by="tag name", value="form")
+            item[0] = form.get_attribute("data-product-sku")
+            ## Recuperer le prix
+            pricing = product.find_element(by="css selector", value="div.pricing")
+            price_div = pricing.find_element(by="css selector", value="span.price.price-final_price.tax.weee")
+            item[1] = price_div.text.replace("€", ",")
+            items.append(item)
+        except: 
+            try:
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.product-item-link.name")))
+                url = product.find_element(by="css selector", value="a.product-item-link.name")
+                no_sku_url.append(url.get_attribute("href"))
+                print("SKU not found placing it in the url array.")
+            except:
+                get_sku()
+
+
+def find_element_with_retry(items, no_sku_url, url, driver):
+    while True:
+        try:
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.pricing")))
+            return
+        except:
+            print("Element not found, retrying...")
+            get_sku(items, no_sku_url, url, driver)
+            continue
